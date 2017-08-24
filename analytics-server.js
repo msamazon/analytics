d6789a57030f64adb07bf964fd762e41a15a2398
DriveonGit <30807887+DriveonGit@users.noreply.github.com>
2017-08-24 11:40:07 -0300

    initial

--- analytics-server.js
+++ analytics-server.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.json({marcelo: 'aaaa'});
+app.set('port', (process.env.PORT || 3000));
 
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
-app.listen(3000, function () {
-  console.log('listening on port 3000!');
+app.listen(app.get('port'), function() {
+  console.log('Application Running on port:', app.get('port'));
 });
