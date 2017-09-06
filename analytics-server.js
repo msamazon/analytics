1e43a0c79cc156f9218dff461241a6e6396c98f2
DriveonGit <30807887+DriveonGit@users.noreply.github.com>
2017-09-06 14:13:21 -0300

    fix map bugs

--- analytics-server.js
+++ analytics-server.js
@@ -9,17 +9,12 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-
 var routes = require(__dirname + '/routes/Routes'); //importing route
 routes(app); //register the route
-
 
 app.use(function(req, res) {
   res.status(404).send({url: req.originalUrl + ' not found'})
 });
-// app.get('/monitor', auth, function(request, response) {
-//   response.render('pages/monitor');
-// });
 
 app.listen(app.get('port'), function() {
   console.log('Application Running on port:', app.get('port'));
